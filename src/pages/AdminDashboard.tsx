40656bb8f1f08e968c2296611e44b75e2b836e66
JoaoSartoreto <joaovitorsartoreto@hotmail.com>
2025-06-23 16:44:01 -0400

    feat: adiciona rota e componente para tela admin

---
+++ src/pages/AdminDashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminDashboard: React.FC = () => {
+  return (
+    <div>
+      <h2>Tela Admin</h2>
+      {/* Aqui você irá construir o layout da tela admin */}
+    </div>
+  );
+};
+
+export default AdminDashboard;
--- src/routes/routesConfig.tsx
+++ src/routes/routesConfig.tsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from '../App.tsx'
 import NotFoundPage from "../pages/notFoundPage.tsx";
+import AdminDashboard from "../pages/AdminDashboard.tsx";
+
 
 
 const router = createBrowserRouter([
     {path: "/", element: <App/>},
+    {path: "/admin", element: <AdminDashboard />},
     {path: "*", element: <NotFoundPage/>},
 ]);
 
