ddc38c125884fd2e92d4fd637cd344d096d69b5e
RMTerayama <rmterayama@gmail.com>
2025-06-24 14:37:20 -0400

    arrumando parte visual do App e Index

--- src/routes/routesConfig.tsx
+++ src/routes/routesConfig.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from '../App.tsx'
 import NotFoundPage from "../pages/notFoundPage.tsx";
-
+import SolicitacaoCracha from "../pages/SolicitacaoCracha.tsx";
 
 const router = createBrowserRouter([
     {path: "/", element: <App/>},
-    {path: "*", element: <NotFoundPage/>},
+    {path: "*", element: <NotFoundPage/>},,
+    {path: "/SolicitacaoCracha", element: <SolicitacaoCracha/>},,
+
 ]);
 
 
