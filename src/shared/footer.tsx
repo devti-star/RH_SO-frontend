5a19819169fa5d0ddf8f0416a6384ff7ba7b142e
Vinicius Barbosa <viniciusbarbosaribeiro12@gmail.com>
2025-06-24 12:58:29 -0400

    add text of footer

---
+++ src/shared/footer.tsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+        return(
+          <footer>
+            <p>Copyright &copy; Prefeitura Municipal de Três Lagoas</p> 
+          </footer>
+        )
+}
+
+export default Footer;
+
