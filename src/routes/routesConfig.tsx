c0359c8f1dafc02b239219e5d2d46985a35fbbe5
RMTerayama <rmterayama@gmail.com>
2025-06-25 08:44:20 -0400

    feat: criação da tela de requerimento de carimbos

--- src/routes/routesConfig.tsx
+++ src/routes/routesConfig.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from '../App.tsx'
 import NotFoundPage from "../pages/notFoundPage.tsx";
-
+import SolicitacaoCarimbo from "../pages/solicitacaoCarimbo.tsx";
 
 const router = createBrowserRouter([
     {path: "/", element: <App/>},
     {path: "*", element: <NotFoundPage/>},
+    { path: "/solicitacaoCarimbo", element: <SolicitacaoCarimbo /> }
+
 ]);
 
 
