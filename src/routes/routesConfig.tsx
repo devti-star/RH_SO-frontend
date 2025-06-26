497da52e3e84f8b5cf40b5c9ba42a04d84731e40
makakof <luiz.h.s.silva@ufms.br>
2025-06-26 12:02:49 -0400

    "Commit base"

--- src/routes/routesConfig.tsx
+++ src/routes/routesConfig.tsx
@@ -4,16 +4,16 @@ import NotFoundPage from "../pages/notFoundPage.tsx";
 import SolicitacaoCarimbo from "../pages/solicitacaoCarimbo.tsx";
 import SolicitacaoCracha from "../pages/SolicitacaoCracha.tsx";
 import Login from "../auth/login.tsx";
+import MeusDados from "../pages/meus-dados/meus-dados.tsx";
 
 
 /* alteraçoes que for feitas aqui devem alterar o shared/header*/
 
 const router = createBrowserRouter([
-
     {path: "/login", element: <Login/>},    
     {path: "/solicitacaoCarimbo", element: <SolicitacaoCarimbo />},
     {path: "/SolicitacaoCracha", element: <SolicitacaoCracha/>},
-
+    {path: "/meus-dados", element: <MeusDados/>},
     {path: "*", element: <NotFoundPage/>},
 ]);
 
