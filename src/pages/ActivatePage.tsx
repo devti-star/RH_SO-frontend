5c0ef00a4d618409a461233b0e6ba431a4bd4acc
Vinicius Barbosa <viniciusbarbosaribeiro12@gmail.com>
2025-07-08 12:23:51 -0400

    finish

--- src/pages/ActivatePage.tsx
+++ src/pages/ActivatePage.tsx
@@ -1,81 +1,81 @@
 import React, { useEffect, useState } from "react";
-import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { Dialog, DialogContent, DialogTitle, IconButton } from "@mui/material";
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from "@mui/icons-material/Close";
 import { apiURL } from "../config";
+import NotFoundPage from "./notFoundPage";
 
 const ActivatePage: React.FC = () => {
-    const { token } = useParams<{ token: string }>();
-    const [success, setSuccess] = useState(false);
-    const [error, setError] = useState<string | null>(null);
-    const navigate = useNavigate();
+  const { token } = useParams<{ token: string }>();
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const activateUser = async () => {
-            try {
-                const response = await axios.get(`${apiURL}/activate/${token}`);
+  useEffect(() => {
+    const activateUser = async () => {
+      try {
+        const response = await axios.get(`${apiURL}/activate/${token}`);
 
-                if (response.status === 202) {
-                    setSuccess(true);
-                    setError(null); // limpa qualquer erro anterior
-                } else {
-                    setSuccess(false);
-                    setError("Erro ao ativar conta.");
-                }
-            } catch (err) {
-                setSuccess(false);
-                setError("Erro ao ativar conta.");
-            }
-        };
+        if (response.status === 202) {
+          setSuccess(true);
+          setError(null);
+        } else {
+          setSuccess(false);
+          setError("Erro ao ativar conta.");
+        }
+      } catch (err) {
+        setSuccess(false);
+        setError("Erro ao ativar conta.");
+      }
+    };
 
-        activateUser();
-    }, [token]);
-
-    const [modalAberto, setModalAberto] = useState(true);
-    const modificarModal = () => setModalAberto(!modalAberto);
-
-    const fecharModal = () => {
-        setModalAberto(!modalAberto);
-        navigate('/login');
+    if (token) activateUser();
+    else {
+      setError("Token inválido");
     }
+  }, [token]);
 
+  const [modalAberto, setModalAberto] = useState(true);
 
-    return (
+  const modificarModal = () => setModalAberto(!modalAberto);
+
+  const fecharModal = () => {
+    setModalAberto(false);
+    navigate("/login");
+  };
+
+  if (error) return <NotFoundPage />;
+
+  return (
+    <>
+      {success && (
         <Dialog
-            open={modalAberto}
-            onClose={modificarModal}
-            maxWidth="sm"
-            fullWidth
-            slotProps={{ paper: { sx: { borderRadius: "15px" } } }}
+          open={modalAberto}
+          onClose={modificarModal}
+          maxWidth="sm"
+          fullWidth
+          slotProps={{ paper: { sx: { borderRadius: "15px" } } }}
         >
-            <DialogTitle
-                sx={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                }}
-            >
-                Verificação de conta
-                <IconButton onClick={fecharModal}>
-                    <CloseIcon />
-                </IconButton>
-            </DialogTitle>
-            {success && (<DialogContent>
-                <p>
-                    Sua conta foi ativada com sucesso.
-                </p>
-            </DialogContent>)
-            }
-            {error && (<DialogContent>
-                <p>
-                    Não foi possivel ativar sua conta.
-                </p>
-            </DialogContent>)
-            }
+          <DialogTitle
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            Verificação de conta
+            <IconButton onClick={fecharModal}>
+              <CloseIcon />
+            </IconButton>
+          </DialogTitle>
+          <DialogContent>
+            <p>Sua conta foi ativada com sucesso.</p>
+          </DialogContent>
         </Dialog>
-
-    );
+      )}
+    </>
+  );
 };
 
 export default ActivatePage;
