7d8956ea39cbac2b2e8c5797cf86f3fb042494bc
RMTerayama <rmterayama.2000@gmail.com>
2025-07-18 14:05:23 -0400

    Foi adicionado um roolbar para que o navbar nao sobreponha o conteudo da pagina

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,13 +1,14 @@
 import ResponsiveAppBar from "../shared/header";
 import Footer from "../shared/footer/footer";
 import { Outlet } from "react-router-dom";
-import { Box } from "@mui/material";
+import { Box, Toolbar } from "@mui/material"; // Importa Toolbar do MUI
 
 export default function Page() {
   return (
     <>
       <Box>
-        <ResponsiveAppBar></ResponsiveAppBar>
+        <ResponsiveAppBar />
+        <Toolbar sx={{ minHeight: { xs: 62, md: 64 } }} /> {/* OFFSET para o header */}
         <Outlet />
         <Footer />
       </Box>
